1262ccf306e21f376163d9d2be727452888a6473
michael thomas <michael.thomas2014@gmail.com>
2020-09-01 19:18:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Add your code here
+<<<<<<< HEAD
 function submitData(name, email) {
     let formData = {
         name: name,
@@ -30,4 +31,38 @@ function submitData(name, email) {
             document.body.appendChild(h3);
             console.log(error.message);
         });
+=======
+function submitData(username, email){
+ let formData = {
+    name: username,
+    email: email
+  };
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+  
+    fetch('http://localhost:3000/users', configObj)
+      .then(function(response) {
+       return response.json();
+      })
+      .then(function(object) {
+        let h2 = document.createElement('h2');
+            h2.innerText = object.id;
+            document.body.appendChild(h2);
+            console.log(object);
+      })
+      .catch(function(error) {
+          let error =  error.message;
+          let message = document.createElement('h2');
+          message.innerText = error
+          document.appendChild(message)
+      });
+      
+    
+>>>>>>> 6f35c8c45121e88bf6b6a8248f4402461bdc7a51
 }
